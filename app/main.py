0a74c0043d0cc3765a4066142b0d14d5f803a964
EvgeniiMunin <35193823+EvgeniiMunin@users.noreply.github.com>
2020-12-20 16:33:17 +0300

    checked intervals in main

--- app/main.py
+++ app/main.py
@@ -1,49 +1,48 @@
 import pandas as pd
-import json
-import requests
-import numpy as np
 import read_preprocess
 import predict
 
 DAYS_BACK = 200
 PRED_HORIZON = 7
 WIN_LEN = 30
-INCUR = 'BTC'
-OUTCUR = 'USD'
+INCUR = "BTC"
+OUTCUR = "USD"
 
-FEATURES = ['high', 'low', 'open', 'volumefrom', 'volumeto', 'close']
-TARGET_COL = 'close'
+FEATURES = ["high", "low", "open", "volumefrom", "volumeto", "close"]
+TARGET_COL = "close"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     df = read_preprocess.parseData(DAYS_BACK, INCUR, OUTCUR)
-    df, X, y_test, dates = read_preprocess.prepare_data(df, TARGET_COL,
-                                                        window_len=WIN_LEN,
-                                                        pred_horizon=PRED_HORIZON)
+    df, X, y_test, dates = read_preprocess.prepare_data(
+        df, TARGET_COL, window_len=WIN_LEN, pred_horizon=PRED_HORIZON
+    )
 
     model = predict.buildLstmModel(X, INCUR, OUTCUR)
     preds = predict.predict(model, X)
     targets = df[TARGET_COL][WIN_LEN:]
-    # preds = preds[PRED_HORIZON:]
-    print(targets)
-    print(preds.shape, y_test.shape)
+    preds = preds[PRED_HORIZON:]
+    # print(targets)
+    # print(preds.shape, y_test.shape)
 
     # create future date column
     pred_dates = []
     for offset in range(1, PRED_HORIZON + 1):
         pred_dates.append(targets.index[-1] + pd.DateOffset(offset))
     pred_index = targets.index[PRED_HORIZON:].append(pd.Index(pred_dates))
-    print(pred_index.shape, pred_index)
+    # print(pred_index.shape, pred_index)
 
-    # denormalize future predictions
+    # denormalize future preds
     preds_denorm = []
     prev_pred = targets[-1]
     for i in range(targets[-PRED_HORIZON:].shape[0]):
         pred_val = targets[-PRED_HORIZON:][i] * (preds[i] + 1)
-        print(targets[-PRED_HORIZON:][i], preds[i], pred_val)
+        # print(targets[-PRED_HORIZON:][i], preds[i], pred_val)
         preds_denorm.append(pred_val)
 
-    out_preds = targets * (preds + 1)
-    print(out_preds.shape, len(preds_denorm))
-    temp = list(out_preds[PRED_HORIZON:]) + preds_denorm
+    # denormalize historic preds on its end of window target value
+    out_preds = targets[:-PRED_HORIZON] * (preds + 1)
+    # print(out_preds.shape, len(preds_denorm), targets.shape, preds.shape)
+    temp = list(out_preds) + preds_denorm
+    # print(pred_index.shape, len(temp))
     out_preds = pd.Series(index=pred_index, data=temp)
     print(out_preds)
--- app/read_preprocess.py
+++ app/read_preprocess.py
@@ -3,35 +3,39 @@ import numpy as np
 import requests
 import json
 
-FEATURES = ['high', 'low', 'open', 'volumefrom', 'volumeto', 'close']
-TARGET_COL = 'close'
+FEATURES = ["high", "low", "open", "volumefrom", "volumeto", "close"]
+TARGET_COL = "close"
+
 
 def parseData(days_back, incur, outcur):
-    endpoint = 'https://min-api.cryptocompare.com/data/histoday'
-    res = requests.get(endpoint + '?fsym={}&tsym={}&limit={}'.format(incur, outcur, days_back))
-    df = pd.DataFrame(json.loads(res.content)['Data'])
-    df = df.set_index('time')
-    df.index = pd.to_datetime(df.index, unit='s')
+    endpoint = "https://min-api.cryptocompare.com/data/histoday"
+    res = requests.get(
+        endpoint + "?fsym={}&tsym={}&limit={}".format(incur, outcur, days_back)
+    )
+    df = pd.DataFrame(json.loads(res.content)["Data"])
+    df = df.set_index("time")
+    df.index = pd.to_datetime(df.index, unit="s")
     return df[FEATURES]
 
+
 def extract_window_data(df, window_len=5, zero_base=True):
-  window_data, dates = [], []
+    window_data, dates = [], []
 
-  # extract window_len points for win segment. get only features
-  copydf = df.copy(deep=True).drop(['close'], axis=1)
+    # extract window_len points for win segment. get only features
+    copydf = df.copy(deep=True).drop(["close"], axis=1)
 
-  for idx in range(len(copydf) - window_len):
-      tmp = copydf[idx: (idx + window_len)].copy(deep=True)
+    for idx in range(len(copydf) - window_len):
+        tmp = copydf[idx : (idx + window_len)].copy(deep=True)
 
-      if zero_base:
-          tmp = normalise_zero_base(tmp)
+        if zero_base:
+            tmp = normalise_zero_base(tmp)
 
-      dates.append(tmp.index)
-      window_data.append(tmp.values)
-      del tmp
+        dates.append(tmp.index)
+        window_data.append(tmp.values)
+        del tmp
 
-  del copydf
-  return np.array(window_data), dates
+    del copydf
+    return np.array(window_data), dates
 
 
 def prepare_data(df, target_col, window_len=10, pred_horizon=5, zero_base=True):
@@ -39,22 +43,24 @@ def prepare_data(df, target_col, window_len=10, pred_horizon=5, zero_base=True):
     X, dates = extract_window_data(df, window_len, zero_base)
 
     # extract targets - the point after each win_len segment + offset of predict_horizon
-    y_test = df[target_col][window_len + pred_horizon:]
+    y_test = df[target_col][window_len + pred_horizon :]
 
     # make offset by value of horizon
     # X = X[:-pred_horizon]
     # dates = dates[:-pred_horizon]
     # df = df[:-pred_horizon]
 
+    # normalize y on its end of window price
     y_test = y_test.values
     if zero_base:
-        y_test = y_test / df[target_col][:-window_len-pred_horizon].values - 1
+        y_test = y_test / df[target_col][: -window_len - pred_horizon].values - 1
 
     return df, X, y_test, dates
 
+
 def normalise_zero_base(df):
-  return df / df.iloc[0] - 1
+    return df / df.iloc[0] - 1
+
 
 def normalise_min_max(df):
-  return (df - df.min()) / (df.max() - df.min())
-
+    return (df - df.min()) / (df.max() - df.min())
