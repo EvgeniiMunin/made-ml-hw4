8b8d117415cdf2d2a687899119dce0abaae99912
EvgeniiMunin <35193823+EvgeniiMunin@users.noreply.github.com>
2020-12-20 15:37:19 +0300

    scripts preprocess predict

--- app/main.py
+++ app/main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import pandas as pd
+import json
+import requests
+import numpy as np
+import read_preprocess
+import predict
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+DAYS_BACK = 200
+PRED_HORIZON = 7
+WIN_LEN = 30
+INCUR = 'BTC'
+OUTCUR = 'USD'
 
+FEATURES = ['high', 'low', 'open', 'volumefrom', 'volumeto', 'close']
+TARGET_COL = 'close'
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    df = read_preprocess.parseData(DAYS_BACK, INCUR, OUTCUR)
+    df, X, y_test, dates = read_preprocess.prepare_data(df, TARGET_COL,
+                                                        window_len=WIN_LEN,
+                                                        pred_horizon=PRED_HORIZON)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    model = predict.buildLstmModel(X, INCUR, OUTCUR)
+    preds = predict.predict(model, X)
+    targets = df[TARGET_COL][WIN_LEN:]
+    # preds = preds[PRED_HORIZON:]
+    print(targets)
+    print(preds.shape, y_test.shape)
+
+    # create future date column
+    pred_dates = []
+    for offset in range(1, PRED_HORIZON + 1):
+        pred_dates.append(targets.index[-1] + pd.DateOffset(offset))
+    pred_index = targets.index[PRED_HORIZON:].append(pd.Index(pred_dates))
+    print(pred_index.shape, pred_index)
+
+    # denormalize future predictions
+    preds_denorm = []
+    prev_pred = targets[-1]
+    for i in range(targets[-PRED_HORIZON:].shape[0]):
+        pred_val = targets[-PRED_HORIZON:][i] * (preds[i] + 1)
+        print(targets[-PRED_HORIZON:][i], preds[i], pred_val)
+        preds_denorm.append(pred_val)
+
+    out_preds = targets * (preds + 1)
+    print(out_preds.shape, len(preds_denorm))
+    temp = list(out_preds[PRED_HORIZON:]) + preds_denorm
+    out_preds = pd.Series(index=pred_index, data=temp)
+    print(out_preds)
---
+++ app/predict.py
@@ -0,0 +1,17 @@
+import pickle
+from keras.layers import Activation, Dense, Dropout, LSTM
+from keras.models import Sequential
+
+def buildLstmModel(X, incur, outcur, output_size=1, neurons=100, activ_func='linear',
+                     dropout=0.2, loss='mse', optimizer='adam'):
+  model = Sequential()
+  model.add(LSTM(neurons, input_shape=(X.shape[1], X.shape[2])))
+  model.add(Dropout(dropout))
+  model.add(Dense(units=output_size))
+  model.add(Activation(activ_func))
+  model.compile(loss=loss, optimizer=optimizer)
+  model.load_weights('saved_models/model_v1_lstm800_mod_win_7d_{}_{}.h5'.format(incur, outcur))
+  return model
+
+def predict(model, X):
+    return model.predict(X).squeeze()
